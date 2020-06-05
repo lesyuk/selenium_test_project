1f8c24728e16e398b8ea1713bdb7b39dd9cf597a
Paul Lesyuk <paul.lesyuk@gmail.com>
2020-06-05 13:21:36 +0300

    Delete test.py

--- common/test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from selenium import webdriver
-from common.helpers import CHROME_DRIVER_PATH
-
-driver = webdriver.Chrome('/Users/paul/chromedriver')
-driver.get("https://www.cian.ru/")
-assert "ЦИАН" in driver.title
